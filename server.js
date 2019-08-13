59fd95a4d7bcf3fc0fef039aa950c014bc12a541
Sam Matthiae <rocketsam13@yahoo.com>
2019-08-12 20:46:41 -0500

    fixed dependencies-goes to local host

--- server.js
+++ server.js
@@ -1,31 +1,50 @@
 var cheerio = require('cheerio');
 var request = require('request'); 
-var bodyParser = require('bodyParser');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var logger = require('logger');
-var exphbs = require('express-handlebars');
-var axios = rewuire('axios');
+var logger = require('morgan');
+var axios = require('axios');
 
-request('http://codedemos.com/sampleblog', (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
+var express = require('express');
+var app = express();
 
-        const siteHeading = $('site-heading');
+app.use(logger("dev"));
+app.use(
+    bodyParser.urlencoded({
+        extended:false
+    })
+);
 
-       //options to fing specific info
-       
-        //console.log(siteHeading.html());
-        //console.log(siteHeading.text());
-        // const output = siteHeading.find('h1').text();
-        // const output = siteHeading.children('h1').text();
-        // const output = siteHeading
-        // .children
-        // .next
-        // .text('h1').text();
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
-
-
-
-
-    }
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("listening on https://localhost:" + port);
 });
+
+
+// request('http://codedemos.com/sampleblog', (error, response, html) => {
+//     if (!error && response.statusCode == 200) {
+//         const $ = cheerio.load(html);
+
+//         const siteHeading = $('site-heading');
+
+//        //options to fing specific info
+       
+//         //console.log(siteHeading.html());
+//         //console.log(siteHeading.text());
+//         // const output = siteHeading.find('h1').text();
+//         // const output = siteHeading.children('h1').text();
+//         // const output = siteHeading
+//         // .children
+//         // .next
+//         // .text('h1').text();
+
+
+
+
+
+//     }
+// });
